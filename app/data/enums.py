e7f5efda89c4855f9853c58625df66a9cb96e311
Paulo Herrera <pauloherreralopes@gmail.com>
2024-01-24 21:05:09 -0300

    Created the model for the MVP

--- app/data/enums.py
+++ app/data/enums.py
@@ -5,3 +5,15 @@ class ListingPurpose(enum.Enum):
     rental = 1
     purchase = 2
     both = 3
+
+
+class UserRole(enum.Enum):
+    announcer = 1
+    broker = 2
+    sysAdmin = 3
+
+
+class PropertyType(enum.Enum):
+    house = "house"
+    apartment = "apartment"
+    commercial = "commercial"
--- app/data/models.py
+++ app/data/models.py
@@ -1,10 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy import String, Enum, Numeric, Boolean, Column
-from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy import (
+    ForeignKey,
+    and_,
+    String,
+    Enum,
+    Numeric,
+    Boolean,
+    Column,
+    DateTime,
+    ARRAY,
+)
+from sqlalchemy.orm import DeclarativeBase, mapped_column, relationship
 from uuid import uuid4
 
-from .enums import ListingPurpose
+from .enums import UserRole, ListingPurpose, PropertyType
 
 
 def get_uuid():
@@ -18,6 +27,66 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = mapped_column(String, primary_key=True, unique=True, default=get_uuid)
+    type = mapped_column(Enum(UserRole), nullable=False)
+    name = mapped_column(String(255), nullable=False)
+    password = mapped_column(String(64), nullable=False)
+    email = mapped_column(String(100), unique=True)
+    phone = mapped_column(String(20), nullable=False)
+
+    terms = relationship("TermAcceptance", back_populates="term_acceptances")
+
+    __mapper_args__ = {"polymorphic_on": type}
+
+
+class Broker(User):
+    __tablename__ = "brokers"
+
+    creci_number = mapped_column(Numeric, unique=True)
+
+    __mapper_args__ = {
+        "polymorphic_identity": UserRole.broker,
+        "polymorphic_on": "type",
+    }
+
+
+class SysManager(User):
+    __tablename__ = "sys_managers"
+
+    __mapper_args__ = {
+        "polymorphic_identity": UserRole.sysAdmin,
+        "polymorphic_on": "type",
+    }
+
+
+class Announcer(User):
+    __tablename__ = "announcers"
+
+    document_number = mapped_column(String(14), unique=True)
+
+    ads = relationship("PropertyAd", back_populates="announcer")
+
+    __mapper_args__ = {
+        "polymorphic_identity": UserRole.announcer,
+        "polymorphic_on": "type",
+    }
+
+
+class TermAcceptance(db.Model):
+    __tablename__ = "term_acceptances"
+
+    id = mapped_column(String, primary_key=True, unique=True, default=get_uuid)
+    term_version = mapped_column(String(20), nullable=False)
+    accepted_at = mapped_column(DateTime(timezone=True))
+    device_id = mapped_column(String(20))
+
+    user_id = mapped_column(ForeignKey("users.id"))
+    user = relationship("User", back_populates="terms")
+
+
 class PropertyAd(db.Model):
     __tablename__ = "property_ads"
 
@@ -25,13 +94,24 @@ class PropertyAd(db.Model):
     neighboorhood = mapped_column(String(64))
     city = mapped_column(String(64))
     listing_purpose = mapped_column(Enum(ListingPurpose), nullable=False)
+    property_type = mapped_column(Enum(PropertyType), nullable=False)
     uf = mapped_column(String(2))
-    mustHaveItems = mapped_column(JSONB)
-    desiredItems = mapped_column(JSONB)
     min_budget = mapped_column(Numeric(precision=20, scale=2))
     max_budget = mapped_column(Numeric(precision=20, scale=2))
-    term_acceptance = mapped_column(Boolean, default=False)
     active = mapped_column(Boolean, default=False)
+    mustHaveItems = mapped_column(ARRAY(String), nullable=True)
+    desiredItems = mapped_column(ARRAY(String), nullable=True)
+
+    # Foreign key to link to Announcer (or any other type)
+    announcer_id = mapped_column(
+        ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE")
+    )
+
+    announcer = relationship(
+        "User",
+        primaryjoin=and_(User.id == announcer_id, User.type == UserRole.announcer),
+        back_populates="ads",
+    )
 
     def __repr__(self):
         return "<id {}>".format(self.id)
